84f306c0ea5cddb051569f0da0a018d4c9e8978d
BAHADIR GURPINAR <bahadir.gurpinar@etstur.com>
2018-09-23 20:03:06 +0300

    result

--- src/main/java/com/nortal/testassignment/Solution.java
+++ src/main/java/com/nortal/testassignment/Solution.java
@@ -8,8 +8,6 @@ public class Solution {
 	
 	public static String penetrate(String base64ObjectInput, String fieldName, int newValue) throws Exception {
 
-
-
 		Object o = fromString(base64ObjectInput, fieldName, newValue);
 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -17,88 +15,44 @@ public class Solution {
 		oos.writeObject( o );
 		oos.close();
 		return Base64.getEncoder().encodeToString(baos.toByteArray());
-
-
-		 //toString(o);
-
-
-
-
-        /*
-		byte[] encodedBytes = base64ObjectInput.getBytes();
-		System.out.println("encodedBytes " + new String(encodedBytes));
-		byte[] decodedBytes = org.apache.commons.codec.binary.Base64.decodeBase64(encodedBytes);
-
-		System.out.println("decodedBytes " + new String(decodedBytes));
-
-		return "";
-
-      */
-
 	}
 
-	private static Object fromString( String s, String field, int newValue ) throws IOException,ClassNotFoundException {
+	private static Object fromString( String s, String field, Object newValue ) throws IOException,ClassNotFoundException {
 		byte [] data = Base64.getDecoder().decode( s );
 		ObjectInputStream ois = new ObjectInputStream(
 				new ByteArrayInputStream(  data ) );
 		Object o  = ois.readObject();
 		ois.close();
 		try {
-			o.getClass().getField("value").set(o, 100);
+			o.getClass().getDeclaredField(field).setAccessible(true);
+			o.getClass().getDeclaredField(field).set(o, newValue);
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		} catch (NoSuchFieldException e) {
 			e.printStackTrace();
 		}
 
-
-
-
 		return o;
 	}
 
-
-	private static String toString( Serializable o ) throws IOException {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream( baos );
-		oos.writeObject( o );
-		oos.close();
-		return Base64.getEncoder().encodeToString(baos.toByteArray());
+	public static class Money implements Serializable {
+		static final long serialVersionUID = 9062282770556489963l;
+		public int value;
 	}
 
 
-
-	class Money implements Serializable {
-		transient private static final long serialVersionUID = 9062282770556489963l;
-		private  int value;
-
-		public int getValue() {
-			return value;
-		}
-
-		public void setValue(int value) {
-			this.value = value;
-		}
-
-		public void writeObject(ObjectOutputStream out) throws  IOException{
-			out.defaultWriteObject();
-			out.writeInt(this.value);
-		}
-
-		public void readObject(ObjectInputStream in) throws  IOException{
-			this.value = in.readInt();
-		}
-	}
-
-
-	class MoneyWithCurrency implements Serializable {
+	public static class MoneyWithCurrency implements Serializable {
 		static final long serialVersionUID = -1474896920071739101l;
+		public int amount;
+		public String currency;
 	}
 
-	class MoneyWithCurrencyAndSerial implements Serializable {
+	public static class  MoneyWithCurrencyAndSerial implements Serializable {
 		static final long serialVersionUID = -4997187403367254147l;
+		public int _amount;
+		public String _currency;
+		public String _serial;
+
 	}
-
-
 	
 }
